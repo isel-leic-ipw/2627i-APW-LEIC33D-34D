0d4abab5e019ad85c00ee6930e03d2c0bc2f8423
Fernanda Passos <fernanda.passos@isel.pt>
2026-09-07 18:42:01 +0100

    Primitive type examples.

---
+++ Week01/primitive-types.js
@@ -0,0 +1,8 @@
+// Primitive types and operators
+let x = BigInt(123);
+let y = 123;
+let z = 123n;
+let a = 10000000000000000 + 1;
+console.log("1:", x, y, z, a);
+console.log("2:", typeof x, typeof y, typeof z , typeof a);
+//console.log("3:", x == y, x == z, x === y, x === z);
